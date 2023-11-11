9b4c0c7c53b2723f3383ae63ab830954b98aefab
mayankfbwork <124149105+mayankfbwork@users.noreply.github.com>
2023-11-11 13:26:34 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://www.bindassbharat.com/graphql"
+	const endpoint = "https://blog.bindassbharat.com/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -18,7 +18,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					'https://www.bindassbharat.com/' + encodeURI(path as string)
+					'https://blog.bindassbharat.com/' + encodeURI(path as string)
 				}`,
 			},
 		};
